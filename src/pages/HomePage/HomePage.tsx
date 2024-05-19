507e2372d6134b669f9cefdcff03fc1e2f1f9036
Matheus <matheus.mleall@gmail.com>
2024-05-19 12:02:59 -0300

    first commit and basic project structure

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { Container, HomePageContainer } from "./styles";
+
+const HomePage = () => {
+	return (
+		<HomePageContainer>
+			<Container></Container>
+		</HomePageContainer>
+	);
+};
+
+export default HomePage;
---
+++ src/pages/HomePage/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HomePageContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+`;
+
+export const Container = styled.div`
+	width: 500px;
+	height: 550px;
+	background-color: #323132;
+	border-radius: 6px;
+`;
